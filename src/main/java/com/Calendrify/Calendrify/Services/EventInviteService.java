48b82c4b7dc11a9b53b932ddbdb0bbce500a2856
Himanshu10219 <himanshus@bbd.co.za>
2023-03-23 17:44:34 +0530

    Clear

--- src/main/java/com/Calendrify/Calendrify/Services/EventInviteService.java
+++ src/main/java/com/Calendrify/Calendrify/Services/EventInviteService.java
@@ -28,9 +28,7 @@ public class EventInviteService {
     {
         Eventinvite list ;
         list = inviteRepo.findById(id).get();
-        if(){
-            return list;
-        }
+        
         return  null;
     }
 }
